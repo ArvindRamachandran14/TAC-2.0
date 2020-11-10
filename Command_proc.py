b476907692082ec30de98c82a51f88704cbd33b0
Arvind <aramac13@asu.edu>
2020-11-10 02:56:11 -0700

    IRGA pump, SC bypass commands added

--- Command_proc.py
+++ Command_proc.py
@@ -52,11 +52,13 @@ class Command_Proc():
                 ###### check if self.strings[1] is a parameter that can actually be set or if it is a readonly paramter
                 if self.strings[1] == "ByPass":
 
-                    print("megaio 0 rwrite 8 "+self.switch[int(self.strings[2])])
+                    os.system("megaio 0 rwrite 8 "+self.switch[int(self.strings[2])])
 
-                    #os.system(print("megaio 0 rwrite 8 "+self.switch[int(self.strings[2])]))
+                    current_time = time.time() # current time 
 
-                    g.gv.dl.setParm(self.strings[1], int(self.strings[2]))
+                    time_stamp = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+
+                    g.gv.dl.setParm(self.strings[1], int(self.strings[2]), time_stamp)
 
                     Output_string = 'e 0'
 
@@ -64,11 +66,13 @@ class Command_Proc():
 
                 elif self.strings[1] == "IRGA_pump":
 
-                    print("megaio 0 rwrite 7 "+self.switch[int(self.strings[2])])
+                    os.system("megaio 0 rwrite 8 "+self.switch[int(self.strings[2])])
 
-                    #os.system(print("megaio 0 rwrite 8 "+self.switch[int(self.strings[2])]))
+                    current_time = time.time() # current time 
 
-                    g.gv.dl.setParm(self.strings[1], int(self.strings[2]))
+                    time_stamp = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+
+                    g.gv.dl.setParm(self.strings[1], int(self.strings[2]), time_stamp)
 
                     Output_string = 'e 0'
 
